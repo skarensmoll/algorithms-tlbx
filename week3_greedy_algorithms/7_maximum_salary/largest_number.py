a02f87d1fca12fa490a266fa05b7c6cf3247551b
Karen Pedraza <karenpedraza@Karens-MacBook-Pro.local>
2021-06-09 23:26:01 +0200

    Solving the following algorithms
    - car_fueling
    - maxium_value_of_the_loot
    - maximum advertisement_revenue
    - collecting signatures
    - maximum number of prizes

--- week3_greedy_algorithms/7_maximum_salary/largest_number.py
+++ week3_greedy_algorithms/7_maximum_salary/largest_number.py
@@ -2,16 +2,37 @@
 
 import sys
 
-def largest_number(a):
-    #write your code here
-    res = ""
-    for x in a:
-        res += x
-    return res
+def greatestNum(a, b):
+    fa, fb = str(a)[0], str(b)[0]
+    la, lb = str(a)[-1], str(b)[-1]
+
+    if fa == fb:
+        return la > lb
+
+    return fa > fb
+
+def largest_number(digits):
+    print(digits)
+    maxDigit = 0
+    salaryNum = ""
+
+    while len(digits) > 0:
+        maxDigit = 0
+        for d in digits:
+            if greatestNum(d,maxDigit):
+                maxDigit = d
+
+        print(maxDigit)
+        digits.remove(maxDigit)
+        salaryNum += str(maxDigit)
+
+    return salaryNum
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
+    #input = "5 9 4 6 1 9"
+    input = "3 23 39 92"
+
     data = input.split()
     a = data[1:]
-    print(largest_number(a))
-    
+    print(largest_number([ int(d) for d in a] ))
+
